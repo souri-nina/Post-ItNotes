0b1252b614af8ca67fff0a93a4c47dac253f85a8
souri.nina@gmail.com <snmoukdahan@gmail.com>
2018-08-30 17:21:16 -0600

    added views, more controller, & form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'notes/index'
-  get 'notes/show'
-  get 'notes/new'
+  
+
+  resources :notes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

309f3052950547487a6fa6c966ab94aaaac94f20
souri.nina@gmail.com <snmoukdahan@gmail.com>
2018-08-30 16:36:25 -0600

    db migrate, added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'notes/index'
+  get 'notes/show'
+  get 'notes/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
